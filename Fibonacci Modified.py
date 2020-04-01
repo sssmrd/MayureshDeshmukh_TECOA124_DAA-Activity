1419c31f63b652c952b663f4d33b589939465d0a
sssmrd <mayureshrd@gmail.com>
2020-04-01 11:21:41 +0530

    Create Fibonacci Modified.py

---
+++ Fibonacci Modified.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    arr=[t1,t2]
+    while(len(arr)!=n):
+        arr.append((arr[len(arr)-1]*arr[len(arr)-1])+arr[len(arr)-2])
+    return arr[len(arr)-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
